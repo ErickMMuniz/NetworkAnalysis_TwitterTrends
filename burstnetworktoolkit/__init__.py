dfc7d4af770556b7de559cc96f4b68b81fb08a8a
Erick Muñiz Morales <erickmuniz@ciencias.unam.mx>
2021-12-13 21:43:04 -0600

    A nwe structure of the code.

---
+++ burstnetworktoolkit/__init__.py
@@ -0,0 +1,24 @@
+"""
+A package to save the principal function to analysis the networks of burst trends.
+"""
+import tarfile
+import pandas as pd
+import numpy as np
+import networkx as nx
+#import networkit as nk
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import datetime as dt
+import collections
+import itertools
+import random
+import seaborn as sn
+import threading
+import os
+import logging
+import pytz
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    print("This is a test")
