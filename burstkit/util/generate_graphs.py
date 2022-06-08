92878211298456203b6364eb783f35e1760fbaf3
Erick Muñiz Morales <erickmuniz@ciencias.unam.mx>
2022-06-07 22:40:23 -0500

    [GenerateGraph] Added a function to generate a graph from dataframe

--- burstkit/util/generate_graphs.py
+++ burstkit/util/generate_graphs.py
@@ -5,3 +5,6 @@ import networkx as nx
 def generate_mutual_follow_graph(limit_rows: "int" = None) -> "nx.Graph":
     edge_df = get_mutual_followers(limit_rows=limit_rows)
     return nx.from_pandas_edgelist(df=edge_df, source="source", target="target")
+
+def generate_graph_from_dataframe(dataframe: "pd.DataFrame", is_directed = False) -> "nx.Graph | nx.Digraph":
+    return nx.from_pandas_edgelist(df=dataframe, source=SOURCE, target=TARGET)
--- main.py
+++ main.py
@@ -10,16 +10,8 @@ from pprint import pprint
 #G: "nx.Graph" = gg.generate_mutual_follow_graph(100)
 
 #pprint(G.number_of_nodes())
-from burstkit.util import SEED
-np.random.seed(SEED)
-some_array = [1,2,3,4,5,6,7,8,9]
-print(np.random.choice(some_array))
-some_dict = {"a": [1,2,3,4],
-             "b": [123,45]}
-print(some_dict.values())
-print(some_dict.keys())
-print(rf.get_relation_trend_timeline_retweets(100)['007april'])
 
-TREND_TIMELINE_TWEETS : "dict[str, pd.DataFrame]" = rf.get_relation_trend_timeline_tweets()
-TREND_TIMELINE_RETWEETS : "dict[str, pd.DataFrame]" = rf.get_relation_trend_timeline_retweets()
-TREND_TIMELINE_MENTIONS : "dict[str, pd.DataFrame]" = rf.get_relation_trend_timeline_mentions()
+
+TREND_TIMELINE_TWEETS : "dict[str, pd.DataFrame]" = rf.get_relation_trend_timeline_tweets(1000)
+#TREND_TIMELINE_RETWEETS : "dict[str, pd.DataFrame]" = rf.get_relation_trend_timeline_retweets()
+#TREND_TIMELINE_MENTIONS : "dict[str, pd.DataFrame]" = rf.get_relation_trend_timeline_mentions()
